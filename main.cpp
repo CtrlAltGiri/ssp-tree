6fb6816ec11ffd948c2c38bdbfd5d9c2ce4461b3
CtrlAltGiri <giridhar.balaz@gmail.com>
2020-09-18 21:50:46 +0530

    Read data success

--- main.cpp
+++ main.cpp
@@ -1,5 +1,11 @@
 #include<iostream>
 #include<vector>
+#include<fstream>
+#include<cstdlib>
+#include<sstream>
+
+#define FILENAME "./data/data.csv"
+
 using namespace std;
 
 struct node{
@@ -14,9 +20,36 @@ struct table{
     node* link;
 };
 
-int main(){
+void read_record(vector<vector<int>> &res)
+{
+	fstream fin(FILENAME, ios::in);
+	string line;
 
+    while(getline(fin, line))
+    {
+        stringstream ss(line);
+        string temp;
+        vector<int> words;
+        char delim = ',';
+        while (std::getline(ss, temp, delim)) {
+            if(!temp.size()){
+                break;
+            }
+            words.push_back(stoi(temp));
+        }
+        res.push_back(words);
+	}
+}
+
+
+
+int main(){
     vector<table> mainTable;
-    
+
+    // Read data
+    vector<vector<int>> data;
+    read_record(data);
+
+    // Start Processing
 
 }
